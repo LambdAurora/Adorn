67d7d51d681e5b2482b65354ef68f04242eaa585
Juuz <6596629+Juuxel@users.noreply.github.com>
2024-08-02 13:52:42 +0300

    Cache services in dev

--- common/src/main/java/juuxel/adorn/util/Services.java
+++ common/src/main/java/juuxel/adorn/util/Services.java
@@ -1,9 +1,18 @@
 package juuxel.adorn.util;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.ServiceLoader;
 
 public final class Services {
+    private static final Map<Class<?>, Object> CACHE = new IdentityHashMap<>();
+
+    @SuppressWarnings("unchecked")
     public static <T> T load(Class<T> c) {
+        return (T) CACHE.computeIfAbsent(c, Services::loadService);
+    }
+
+    private static <T> T loadService(Class<T> c) {
         return ServiceLoader.load(c)
             .findFirst()
             .orElseThrow(() -> new RuntimeException("Could not find Adorn platform service " + c.getSimpleName()));
